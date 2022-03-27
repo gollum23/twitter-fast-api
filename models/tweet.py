2853dddf838de541d144bfd4d97e54f81e92e449
Diego Forero <gollum23@gmail.com>
2022-03-27 05:58:09 -0500

    🎉 Models
    Create User and Tweet models

---
+++ models/tweet.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+from models.user import User
+
+
+class Tweet(BaseModel):
+    user_id: UUID = Field(...)
+    content: str = Field(..., min_length=1, max_length=256)
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    author: User = Field(...)
---
+++ models/user.py
@@ -0,0 +1,32 @@
+from datetime import date
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64,
+    )
+
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+    )
+    birthday: Optional[date]
